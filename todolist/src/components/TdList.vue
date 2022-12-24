cffafa51f3ca5b7b858ce771b1122fd83ec5ec4f
Jinx-FX <2674802603@qq.com>
2022-12-24 19:53:06 +0800

    todolist

---
+++ todolist/src/components/TdList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="todo-main">
+    <TdItem />
+  </ul>
+</template>
+
+<script>
+  import TdItem from './TdItem'
+
+  export default {
+    name: "TdList",
+    components: {TdItem}
+  }
+</script>
+
+<style scoped>
+  /*main*/  
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
