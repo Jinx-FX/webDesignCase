2f1d1433250b0f2709b762f713187e0c692a1271
Jinx-FX <2674802603@qq.com>
2023-02-16 11:14:21 +0800

    瀑布流布局原生实现初步完成，缺少细节优化

--- WaterfallLayout/JS + 懒加载方法/newWaterfall.js
+++ WaterfallLayout/JS + 懒加载方法/newWaterfall.js
@@ -1,8 +1,6 @@
 const waterfall = document.querySelector("#waterfall")
 const pics = document.querySelectorAll("li")
 
-console.log(pics.length)
-
 const newWaterfall = (options = {
   width: 360,
   delay: 100,
@@ -14,7 +12,7 @@ const newWaterfall = (options = {
     if (getScrollTop() + getClient().height > pic.offsetHeight) {
       pic.classList.add("show")
     } else {
-      if (config.repeatShow) {
+      if (options.repeatShow) {
         pic.classList.remove("show")
       }
     }
@@ -27,69 +25,76 @@ const newWaterfall = (options = {
       pics_col = pics.length
     }
 
+    let pic_left = (getClient().width - pics_col * options.width) / 2
+
     // 基础样式设置
     waterfall.style.position = "relative"
-    waterfall.classList.remove("min")
-    pics.style.position = "absolute"
-    pics.style.width = options.width
 
-    // 变量
-    let maxHeight = 0
-    let list = []
-    let nlist = []
+    if (pics_col > 0) {
+      waterfall.classList.remove("min")
 
-    // 初始化列表
-    for (let i = 0; i < pics.length; i++) {
-      list.push({
-        index: i,
-        bottom: 0,
-        height: pics[i].offsetHeight
-      })
-    }
+      // 变量
+      let maxHeight = 0
+      let list = []
+      let nlist = []
 
-    // 初始化列
-    for (let i = 0; i < pics_col; i++) {
-      nlist.push([])
-    }
+      // 初始化列表,基础样式设置 pic
+      for (let i = 0; i < pics.length; i++) {
+        pics[i].style.position = "absolute"
+        pics[i].style.width = options.width + "px"
+        list.push({
+          index: i,
+          bottom: 0,
+          height: pics[i].offsetHeight + "px"
+        })
+      }
 
-    // 智能排序
-    for (let i = 0; i < pics.length; i++) {
-      if (i < pics_col) {
-        list[i]["bottom"] = list[i]["height"]
-        nlist[i].push(list[i])
-      } else {
-        let b = 0
-        let l = 0
-        for (let j = 0; j < pics_col; j++) {
-          let jh = nlist[j][nlist[j].length - 1]["bottom"] + list[i]["height"]
-          if (b == 0 || jh < b) {
-            b = jh
-            l = j
+      // 初始化列
+      for (let i = 0; i < pics_col; i++) {
+        nlist.push([])
+      }
+
+      // 智能排序
+      for (let i = 0; i < pics.length; i++) {
+        if (i < pics_col) {
+          list[i]["bottom"] = list[i]["height"]
+          nlist[i].push(list[i])
+        } else {
+          let b = 0
+          let l = 0
+          for (let j = 0; j < pics_col; j++) {
+            let jh =
+              nlist[j][nlist[j].length - 1]["bottom"] + list[i]["height"]
+            if (b == 0 || jh < b) {
+              b = jh
+              l = j
+            }
           }
+          list[i]["bottom"] = b
+          nlist[l].push(list[i])
         }
-        list[i]["bottom"] = b
-        nlist[l].push(list[i])
       }
+
+      // 开始布局
+      for (let i = 0; i < nlist.length; i++) {
+        for (let j = 0; j < nlist[i].length; j++) {
+          pics[nlist[i][j]["index"]].style.left =
+            i * options.width + pic_left + "px"
+          pics[nlist[i][j]["index"]].style.top =
+            nlist[i][j]["bottom"] - nlist[i][j]["height"] + "px"
+        }
+      }
+
+      // 设置最大高度
+      for (let i = 0; i < nlist.length; i++) {
+        let h = nlist[i][nlist[i].length - 1]["bottom"] + "px"
+        if (maxHeight < h) {
+          maxHeight = h
+        }
+      }
+      waterfall.style.height = maxHeight + "px"
     }
 
-    // 开始布局
-    for (let i = 0; i < nlist.length; i++) {
-      for (let j = 0; j < nlist[i].length; j++) {
-        pics[nlist[i][j]["index"]].style.left =
-          i * options.width + pic_left
-        pics[nlist[i][j]["index"]].style.top =
-          nlist[i][j]["bottom"] - nlist[i][j]["height"]
-      }
-    }
-
-    // 设置最大高度
-    for (let i = 0; i < nlist.length; i++) {
-      let h = nlist[i][nlist[i].length - 1]["bottom"]
-      if (maxHeight < h) {
-        maxHeight = h
-      }
-    }
-    waterfall.style.height = maxHeight
 
     // 显示列表
     for (let i = 0; i < pics.length; i++) {
@@ -104,9 +109,9 @@ const newWaterfall = (options = {
 }
 
 // 文档加载完毕后执行
-waterfall.addEventListener("load", newWaterfall)
+window.addEventListener("load", newWaterfall)
 //当页面尺寸发生变化时，触发函数，实现响应式
-waterfall.addEventListener("resize", newWaterfall)
+window.addEventListener("resize", newWaterfall)
 
 // 自动加载更多
 const random = (min, max) => {
