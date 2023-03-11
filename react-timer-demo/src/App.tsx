fea00fcd6dec9db399c387c45378bf738c1cc931
Jinx-FX <2674802603@qq.com>
2023-03-11 12:29:38 +0800

    完善了大部分需求，边界问题待处理

--- react-timer-demo/src/App.tsx
+++ react-timer-demo/src/App.tsx
@@ -1,40 +1,92 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { useCountDown } from './hooks'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const Login = () => {
+  // 发送验证码
+  // 设置初始值  定时器结束时候可以继续点击
+  const { count, start } = useCountDown(10, () => setSendAble(true))
+  // 默认刚开始可以点击
+  const [sendAble, setSendAble] = useState(true)
+  const onSendCode = () => {
+    // 开启定时器
+    start()
+    // 关闭
+    setSendAble(false)
+  }
+
+  return (
+    <button className="code-extra" onClick={onSendCode}>
+      {sendAble ? '发送验证码' : count + '秒之后发送'}
+    </button>
+  )
+}
+
+const Vaild = () => {
+  const [cansend, setcansend] = useState(true)
+  const [timer, settimer] = useState(3)
+  const time = useRef(null)
+  // 点击事件
+  const send = () => {
+    // 初始赋值给3
+    settimer(3)
+    // 1.禁用按钮
+    setcansend(false)
+    // 2.开启定时器
+    time.current = setInterval(() => {
+      settimer((timer) => timer - 1)
+    }, 1000)
+  }
+  useEffect(() => {
+    console.log('当前timer为', timer)
+    // 如果时间为0
+    if (timer === 0) {
+      // 1.解除禁用
+      setcansend(true)
+      // 2.清除定时器
+      clearInterval(time.current)
+    }
+  }, [timer])
+  return (
+    <div>
+      <input type="text" name="" id="" />
+      <button disabled={!cansend} onClick={send}>
+        {cansend ? '发送验证码' : timer + '秒后重发'}
+      </button>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState('10s')
+  const [canclick, setCanclick] = useState(false)
 
   useEffect(() => {
     let num = 10
 
     // by setTimeout
-    // let timerId = setTimeout(function go() {
-    //   if (parseInt(count) > 0) {
-    //     setTimeout(go, 1000)
-    //   }
-    //   if (count == '已抢购') {
-    //     console.log('111')
-    //     num = -1
-    //     clearTimeout(timeId)
-    //   } else if (num === 0) {
-    //     console.log('333')
-    //     clearTimeout(timeId)
-    //     num--
-    //     setCount('抢购')
-    //   } else if (num > 0) {
-    //     setCount(num-- + 's')
-    //     console.log('222')
-    //   }
-    // }, 1000)
+    let timerId = setTimeout(function go() {
+      if (parseInt(count) > 0) {
+        setTimeout(go, 1000)
+      }
+      if (num === 0) {
+        clearTimeout(timerId)
+        num--
+        setCanclick(true)
+        setCount('抢购')
+      } else if (num > 0) {
+        setCount(num-- + 's')
+      }
+    }, 1000)
 
     // by setInterval
-    const timerId = setInterval(function () {
-      if (num == 0) {
-        clearInterval(timerId)
-      }
-      setCount(num-- + 's')
-    }, 1000)
+    // const timerId = setInterval(function () {
+    //   if (num == 0) {
+    //     clearInterval(timerId)
+    //   }
+    //   setCount(num-- + 's')
+    // }, 1000)
 
     timerId
   }, [])
@@ -54,15 +106,17 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card" onClick={onSumbit}>
-        <button>{count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="card">
+        <button disabled={!canclick} onClick={onSumbit}>
+          {count}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <Login />
+      </div>
+      <div className="card">
+        <Vaild />
+      </div>
     </div>
   )
 }
